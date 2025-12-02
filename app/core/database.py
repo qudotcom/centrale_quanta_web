5ec7a8da48bae9cd741b108cae1a4b1ca3deb76b
abdelouahed TAHRI <202654429+qudotcom@users.noreply.github.com>
2025-12-02 14:11:27 +0100

    Set up SQLAlchemy database connection and session

--- app/core/database.py
+++ app/core/database.py
@@ -1,1 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+# The file will be created in the data folder
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/quanta.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False} # Needed for SQLite
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Dependency to use in routes
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
